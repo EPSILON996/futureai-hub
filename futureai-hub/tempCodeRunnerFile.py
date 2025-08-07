9d858242e6c799c7c33bd4558bc4d6c0f4e8e9a5
Mayur Ubarhande <mayurubarhande996@gmail.com>
2025-08-07 09:01:15 +0530

    Add files via upload

---
+++ futureai-hub/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+app.config['MAIL_USERNAME'] = 'mayurubarhande996@gmail.com'          # <-- your email
+app.config['MAIL_PASSWORD'] = 'xgfq aoml horb txpc'       # <-- your app password
+app.config['MAIL_DEFAULT_SENDER'] = 'mayurubarhande996@gmail.com'
