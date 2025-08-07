f96b390f764b9c9d19bbaf9ce4f7f597e253a9e4
Mayur Ubarhande <mayurubarhande996@gmail.com>
2025-08-07 08:58:18 +0530

    Delete futureai-hub/futureai-hub directory

--- futureai-hub/futureai-hub/tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-app.config['MAIL_USERNAME'] = 'mayurubarhande996@gmail.com'          # <-- your email
-app.config['MAIL_PASSWORD'] = 'xgfq aoml horb txpc'       # <-- your app password
-app.config['MAIL_DEFAULT_SENDER'] = 'mayurubarhande996@gmail.com'
